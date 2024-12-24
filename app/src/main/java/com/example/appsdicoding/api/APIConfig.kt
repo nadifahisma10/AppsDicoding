6919575978136e97e0011aed521c1a02fbc2a06a
nadifahisma10 <129817146+nadifahisma10@users.noreply.github.com>
2024-12-24 19:11:39 +0700

    Create APIConfig.kt

---
+++ app/src/main/java/com/example/appsdicoding/api/APIConfig.kt
@@ -0,0 +1,29 @@
+package com.example.appsdicoding.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object APIConfig {
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private const val BASE_URL = "https://event-api.dicoding.dev/"
+
+    // Menggunakan lazy untuk inisialisasi Retrofit hanya ketika diperlukan
+    val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
